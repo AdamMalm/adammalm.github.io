ff6fe142388e905039a73ddc53d1247ef6de6f54
AdamMalm <adam.hornemalm@gmail.com>
2021-05-03 16:16:06 +0200

    Almost done

---
+++ dist/main.js
@@ -0,0 +1,30 @@
+window.addEventListener('scroll', () => {
+    myID = document.getElementById("back2top");
+
+    var y = window.scrollY;
+    console.log(y);
+    if (y >= 900) {
+        myID.className = "show"
+    } else {
+        myID.className = "hide"
+        var x = document.getElementById("hamburger-menu");
+        x.className = "hamburger-menu-hide";
+    }
+});
+
+function myFunction() {
+    var x = document.getElementById("hamburger-menu");
+    if (x.className === "hamburger-menu-hide") {
+        x.className = "hamburger-menu-show";
+    } else {
+        x.className = "hamburger-menu-hide";
+    }
+}
+
+
+
+
+
+
+
+        
